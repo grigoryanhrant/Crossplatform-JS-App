d0daf36ca7d19d38c4df171099ade7b2ae60a7a5
grigoryanhrant <hgrigoryan755@gmail.com>
2023-03-08 07:37:56 -0800

    apollo server

--- projects/notedly/api/src/index.js
+++ projects/notedly/api/src/index.js
@@ -1,2 +1,43 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const port = process.env.PORT || 4000;
+
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
+const typeDefs = gql`  
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+ },
+ 
+ type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+ }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+    notes: () => notes,
+  }
+};
+
+const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+)
