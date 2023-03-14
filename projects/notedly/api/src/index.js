324a02338e5646065fc275806b66104f45786108
grigoryanhrant <73654151+grigoryanhrant@users.noreply.github.com>
2023-03-14 21:20:49 +0400

    connect to db

--- projects/notedly/api/src/index.js
+++ projects/notedly/api/src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
-
+require('dotenv').config();
+const db = require('./db');
 const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
 
 let notes = [
   { id: '1', content: 'This is a note', author: 'Adam Scott' },
@@ -40,7 +42,6 @@ const resolvers = {
         content: args.content,
         author: 'Adam Scott',
       };
-      console.log(notes)
       notes.push(noteValue)
       return noteValue
     }
@@ -48,6 +49,8 @@ const resolvers = {
 };
 
 const app = express();
+
+db.connect(DB_HOST);
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
